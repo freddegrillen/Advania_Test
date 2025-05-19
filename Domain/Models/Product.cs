5f29d7a772e12895c2eb7d6cf024e757824bd49a
Fredrik Grill <fredrik.grill.3005@student.uu.se>
2025-05-19 09:38:28 +0200

    added product-model

---
+++ Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advania_Test.Domain.Models
+{
+    public sealed class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Color { get; set; }
+        public decimal Price { get; set; }
+    }
+}
